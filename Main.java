473f93a4d1c171ba068e0020761d09b9b46cc3c7
JakubKwiatek132 <126687451+JakubKwiatek132@users.noreply.github.com>
2023-03-29 13:30:40 +0000

    liczby losowe

--- Main.java
+++ Main.java
@@ -1,15 +1,17 @@
+import java.util.Random;
+
 class Main {
   public static void main(String[] args) {
-    for (int i = 1; i<= 10; i++){
-      for (int j = 1; j<=10; j++){
-        int a = i * j;
-        if (a < 10)
-          System.out.println(" " + i * j);
-        else
-          System.out.println(i * j);
-        System.out.println(" ");
-      }
-      System.out.println("");
+
+  int[][] array = new int [10][10];
+    Random rand = new Random();
+    
+    for(int i = 0; i<10; i++){
+      for(int j =0; j<10; j++){
+      array[i][j] = rand.nextInt(101) -50;
+      System.out.print(array[i][j]+ "\t");
+    }
+    System.out.println();
     }
   }
 }
